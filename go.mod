c98a8fb1236d03c70470da678bacf43d3bc22da6
Andrew Cichewicz <apcichewicz@gmail.com>
2025-01-12 17:19:20 -0500

    add repos

--- go.mod
+++ go.mod
@@ -3,17 +3,27 @@ module github.com/apcichewicz/gotodolist
 go 1.23.4
 
 require (
-	github.com/fatih/color v1.18.0 // indirect
-	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
+	github.com/gofiber/fiber/v2 v2.52.6
+	github.com/golang-jwt/jwt/v5 v5.2.1
+	github.com/uptrace/bun v1.2.8
+	github.com/uptrace/bun/dialect/pgdialect v1.2.8
+	github.com/uptrace/bun/driver/pgdriver v1.2.8
+)
+
+require (
+	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/klauspost/compress v1.17.9 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/puzpuzpuz/xsync/v3 v3.4.0 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/tmthrgd/go-hex v0.0.0-20190904060850-447a3041c3bc // indirect
-	github.com/uptrace/bun v1.2.8 // indirect
-	github.com/uptrace/bun/dialect/pgdialect v1.2.8 // indirect
-	github.com/uptrace/bun/driver/pgdriver v1.2.8 // indirect
-	github.com/uptrace/bun/extra/bundebug v1.2.8 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.51.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
 	github.com/vmihailenco/msgpack/v5 v5.4.1 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
 	golang.org/x/crypto v0.31.0 // indirect
--- repos/todorepo.go
+++ repos/todorepo.go
@@ -7,41 +7,41 @@ import (
 	"github.com/uptrace/bun"
 )
 
-type TodoRepository struct {
+type ToDoRepository struct {
 	db *bun.DB
 }
 
-func newTodoRepo(db *bun.DB) TodoRepository {
-	return TodoRepository{db: db}
+func newToDoRepo(db *bun.DB) ToDoRepository {
+	return ToDoRepository{db: db}
 }
 
-func (tr TodoRepository) GetAllTodos() ([]models.ToDo, error) {
+func (tr ToDoRepository) GetAllToDos() ([]models.ToDo, error) {
 	ctx := context.Background()
 	var todos []models.ToDo
 	err := tr.db.NewSelect().Model(&todos).Scan(ctx)
 	return todos, err
 }
 
-func (tr TodoRepository) GetTodoByID(id int) (models.ToDo, error) {
+func (tr ToDoRepository) GetToDoByID(id int) (models.ToDo, error) {
 	ctx := context.Background()
 	var todo models.ToDo
 	err := tr.db.NewSelect().Model(&todo).Where("id = ?", id).Scan(ctx)
 	return todo, err
 }
 
-func (tr TodoRepository) CreateTodo(todo models.ToDo) (models.ToDo, error) {
+func (tr ToDoRepository) CreateToDo(todo models.ToDo) (models.ToDo, error) {
 	ctx := context.Background()
 	_, err := tr.db.NewInsert().Model(&todo).Exec(ctx)
 	return todo, err
 }
 
-func (tr TodoRepository) UpdateTodo(todo models.ToDo) (models.ToDo, error) {
+func (tr ToDoRepository) UpdateToDo(todo models.ToDo) (models.ToDo, error) {
 	ctx := context.Background()
 	_, err := tr.db.NewUpdate().Model(&todo).WherePK().Exec(ctx)
 	return todo, err
 }
 
-func (tr TodoRepository) DeleteTodo(id int) error {
+func (tr ToDoRepository) DeleteToDo(id int) error {
 	ctx := context.Background()
 	_, err := tr.db.NewDelete().Model(&models.ToDo{}).Where("id = ?", id).Exec(ctx)
 	return err
